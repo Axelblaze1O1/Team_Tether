fe397e323a7e6b77d9594213137861452db86f4a
Axelblaze <teotiaarnav3@gmail.com>
2024-10-30 13:03:19 +0530

    websocket fixes

--- team_tether/consumers.py
+++ team_tether/consumers.py
@@ -8,13 +8,14 @@ django.setup()
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 from base.models import Message, Room,User
+from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['pk']
         self.room_group_name = f"chat_{self.room_name}"
 
-        # Join room group
+        # Join room groups
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -32,10 +33,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         message = data['message']
         username = self.scope["user"].username  # If using auth
-
+        room = self.room_name
+        userid = self.scope["user"].id
         # Save message to database (optional)
         # Add code here to save 'message' to your Message model in the database
-
+        await self.save_message(username,room,message)
         # Broadcast message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -43,15 +45,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "type": "chat_message",
                 "message": message,
                 "username": username,
+                "userid":userid
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
         username = event['username']
+        userid = event['userid']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             "message": message,
             "username": username,
+            "userid":userid
         }))
+    
+    @sync_to_async
+    def save_message(self,username,room_name,message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(id=room_name)
+        Message.objects.create(user=user,room=room,body=message)
